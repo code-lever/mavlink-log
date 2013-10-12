4f922e386577bb9658c886d35626e9ad957d97cb
Nick Veys <nveys@aramisgroup.com>
2013-10-12 16:01:11 -0500

    Adding GpsRawInt

--- lib/mavlink/log/messages/gps_raw_int.rb
+++ lib/mavlink/log/messages/gps_raw_int.rb
@@ -2,6 +2,53 @@ module MAVLink; module Log; module Messages
 
   class GpsRawInt < TimedMessageMicro
 
+    def initialize(entry)
+      super
+    end
+
+    # WGS84 dec. degrees
+    def lat
+      @lat ||= (int32_t(8..11) / 10000000.0)
+    end
+
+    # WGS84 dec. degrees
+    def lon
+      @lon ||= (int32_t(12..15) / 10000000.0)
+    end
+
+    # meters
+    def alt
+      @alt ||= (int32_t(16..19) / 1000.0)
+    end
+
+    # meters
+    def eph
+      @eph ||= (uint16_t(20..21) / 100.0)
+    end
+
+    # meters
+    def epv
+      @epv ||= (uint16_t(22..23) / 100.0)
+    end
+
+    # m/s
+    def vel
+      @vel ||= (uint16_t(24..25) / 100.0)
+    end
+
+    # degrees 0..359.99
+    def cog
+      @cog ||= (uint16_t(26..27) / 100.0)
+    end
+
+    def fix_type
+      @fix_type ||= uint8_t(28)
+    end
+
+    def satellites_visible
+      @satellites_visible ||= uint8_t(29)
+    end
+
   end
 
 end; end; end
---
+++ spec/messages/gps_raw_int_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe MAVLink::Log::Messages::GpsRawInt do
+
+  let(:time) { ["0004d9ee608d5a08"].pack('H*') }
+
+  let(:header) { MAVLink::Log::Header.new(["FE1E9C370118"].pack('H*')) }
+
+  let(:payload) { ["a091a127000000003066b11751535bc1959402000800ffff0500232d0305"].pack('H*') }
+
+  let(:crc) { ["c13a"].pack('H*') }
+
+  subject do
+    entry = MAVLink::Log::Entry.new(time, header, payload, crc)
+    MAVLink::Log::Messages::GpsRawInt.new(entry)
+  end
+
+  it_behaves_like 'a message'
+
+  it_behaves_like 'a timed message (us)'
+
+  its(:fix_type) { should eql(3) }
+
+  its(:lat) { should be_within(0.0001).of(39.7502) }
+
+  its(:lon) { should be_within(0.0001).of(-105.0979) }
+
+  its(:alt) { should be_within(0.01).of(169.11) }
+
+  its(:eph) { should be_within(0.01).of(0.08) }
+
+  its(:epv) { should be_within(0.01).of(655.35) }
+
+  its(:vel) { should be_within(0.01).of(0.05) }
+
+  its(:cog) { should be_within(0.01).of(115.55) }
+
+  its(:satellites_visible) { should eql(5) }
+
+end
